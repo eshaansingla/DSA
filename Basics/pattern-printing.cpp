7094a1a0ce88d6708c82b3c51eb91b5e149ea331
Eshaan Singla <147623220+eshaansingla@users.noreply.github.com>
2024-08-06 13:13:38 +0530

    Update pattern-printing.cpp

--- Basics/pattern-printing.cpp
+++ Basics/pattern-printing.cpp
@@ -1,5 +1,19 @@
 #include<iostream>
 using namespace std;
+// void pattern6(){
+//   for(int i=1;i<=n;i++){
+//         for(int j=1;j<=i;j++){
+//             cout<<j<<" ";
+//         }
+//         for(int k=1;k<=(2*n)-(2*i);k++){
+//             cout<<" ";
+//         }
+//         for(int j=i;j>=1;j--){
+//             cout<<j<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
 // void pattern5(){
 //   for(int i=0;i<n;i++){
 //        for(int l=0;l<(n-i-1);l++){

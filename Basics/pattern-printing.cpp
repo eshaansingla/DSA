615c01535e203dec4008477f1f931298cff54f36
Eshaan Singla <147623220+eshaansingla@users.noreply.github.com>
2024-08-06 13:03:41 +0530

    Update pattern-printing.cpp

--- Basics/pattern-printing.cpp
+++ Basics/pattern-printing.cpp
@@ -26,6 +26,17 @@ using namespace std;
 //        cout << endl;
 //     }
 // }
+// void pattern4(){
+//    int s=1;
+//     for(int i=0;i<n;i++){
+//         if(i%2==0) s=1;
+//         else s=0;
+//     for(int j=0;j<=i;j++){
+//         cout<<s<<" ";
+//         s=1-s;
+//     }
+//     cout<<endl;
+// }
 // void pattern3(){
 //    for(int i=0;i<n;i++){
 //        for (int l=1;l<=i;l++){

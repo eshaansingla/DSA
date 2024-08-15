2d32ce16642e695f14f9b6825862fdc175f117f9
eshaansingla <147623220+eshaansingla@users.noreply.github.com>
2024-08-15 11:41:58 +0530

    Hash-basics

--- Basics/STL-Basics/Hashing.cpp
+++ Basics/STL-Basics/Hashing.cpp
@@ -1,5 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int n;
+    cout<<"Enter number of elements:";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cout<<"Enter element number "<<i+1<<":";
+        cin>>arr[i];
+    }
+    int m=*max_element(arr,arr+n);
+    int hash[m+1];
+    for(int i=0;i<m+1;i++){
+        hash[i]=0;
+    }
+    for(int i=0;i<n;i++){
+        hash[arr[i]]+=1;
+    }
+    int num;
+    while(1>0){
+        cout<<endl<<"Enter number to find:";
+        cin>>num;
+        if(num>m) cout<<0;
+        else cout<<hash[num];
+    }
 }

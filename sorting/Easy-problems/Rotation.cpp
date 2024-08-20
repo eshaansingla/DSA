83138a1eea20f14274fa68542255ea444c388931
eshaansingla <147623220+eshaansingla@users.noreply.github.com>
2024-08-20 15:35:14 +0530

    right rot

--- sorting/Easy-problems/Rotation.cpp
+++ sorting/Easy-problems/Rotation.cpp
@@ -32,12 +32,17 @@ int main(){
     for(int i=1;i<=100;i++){
         v.push_back(i);
     }
+    int n=sizeof(v);
     int k;
     cout<<"Enter rotations:";
     cin>>k;
     k=k%100;
+    reverse(v.begin()+k,v.end());
+    reverse(v.begin(),v.begin()+k);
     reverse(v.begin(),v.end());
+    /*reverse(v.begin(),v.end());
     reverse(v.begin(),v.begin()+k);
     reverse(v.begin()+k,v.end());
+    */
     for(auto it:v) cout<<it<<" ";   
 }

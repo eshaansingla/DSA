532c9577d3d3d847517d7ccf61ff19cfa63c2550
eshaansingla <147623220+eshaansingla@users.noreply.github.com>
2024-08-20 11:42:36 +0530

    brute rotation

--- sorting/Easy-problems/Rotation.cpp
+++ sorting/Easy-problems/Rotation.cpp
@@ -1,6 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    /*int n;
+    cout<<"Enter number of elements:";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cout<<"Enter a number:";
+        cin>>arr[i];
+    }
+    int d;
+    cout<<"Enter number of rotations:";
+    cin>>d;
+    d=d%n;
+    vector<int>temp;
+    for(int i=0;i<d;i++){
+        temp[i]=arr[i];
+    }
+    for(int i=d;i<n;i++){
+        arr[i-d]=arr[i];
+    }
+    for(int i=n-d;i>=0;i--){
+        arr[i]=temp[i-(n-d)];
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    */
     vector<int>v;
     for(int i=1;i<=100;i++){
         v.push_back(i);
@@ -12,5 +38,5 @@ int main(){
     reverse(v.begin(),v.end());
     reverse(v.begin(),v.begin()+k);
     reverse(v.begin()+k,v.end());
-    for(auto it:v) cout<<it<<" ";
+    for(auto it:v) cout<<it<<" ";   
 }

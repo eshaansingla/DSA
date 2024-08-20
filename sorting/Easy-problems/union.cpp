8cd14b5f75ad9466bc7d93ec8037e5f41b76efe5
eshaansingla <147623220+eshaansingla@users.noreply.github.com>
2024-08-20 19:27:49 +0530

    union-optimum

--- sorting/Easy-problems/union.cpp
+++ sorting/Easy-problems/union.cpp
@@ -17,12 +17,44 @@ int main(){
         cout<<"Enter value:";
         cin>>v1[i];
     }
- set<int>st;
+    sort(v.begin(),v.end());
+    sort(v1.begin(),v1.end());
+ /*set<int>st;
     for(int i=0;i<n;i++){
         st.insert(v[i]);
 }
  for(int i=0;i<m;i++){
         st.insert(v1[i]);
 }
-for(auto it:st)cout<<it<<endl;
-}
+for(auto it:st)cout<<it<<endl;*/
+    vector<int>unionarr;
+    int i=0;
+    int j=0;
+    while(i<n && j<m){
+        if(v[i]<=v1[j]){
+            if(unionarr.size()==0 || unionarr.back()!=v[i]){
+                unionarr.push_back(v[i]);
+            }
+             i++;
+        }
+        else{
+            if(unionarr.size()==0 || unionarr.back()!=v1[j]){
+                unionarr.push_back(v1[j]);
+            }
+             j++;
+        }
+    }
+    while(i<n){
+            if(unionarr.size()==0 || unionarr.back()!=v[i]){
+                unionarr.push_back(v[i]);
+            }
+             i++;
+        }
+        while(j<m){
+             if(unionarr.size()==0 || unionarr.back()!=v1[j]){
+                unionarr.push_back(v1[j]);
+            }
+             j++;
+        }
+        for(auto it:unionarr) cout<<it<<endl;
+        }

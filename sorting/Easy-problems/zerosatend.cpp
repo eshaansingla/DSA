4ef05a520bbcdd17a8f5633a35842232237d9cbb
eshaansingla <147623220+eshaansingla@users.noreply.github.com>
2024-08-20 15:58:51 +0530

    0

--- sorting/Easy-problems/zerosatend.cpp
+++ sorting/Easy-problems/zerosatend.cpp
@@ -1,24 +1,47 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    /* int n;
+    cout<<"Enter n:";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cout<<"Enter value:";
+        cin>>arr[i];
+    }
+    int cnt=0;
+    int j=-1;
+    for (int i=0;i<n;i++) {
+        if (arr[i] == 0) {
+            j=i;
+            break;
+        }
+    }
+    for(int i=j+1;j<n;j++){
+        if(arr[i]!=0) swap(arr[i],arr[j]);
+    }
+    for(auto it:arr) cout<<it<<endl;
+*/
     int n;
     cout<<"Enter n:";
     cin>>n;
-    vector<int>v(n);
+    int arr[n];
     for(int i=0;i<n;i++){
         cout<<"Enter value:";
-        cin>>v[i];
+        cin>>arr[i];
     }
-    int cnt=0;
-    for (int i = n - 1; i >= 0; i--) {
-        if (v[i] == 0) {
-            v.erase(v.begin() + i);
-            cnt++;
+    int j=-1;
+    for (int i=0;i<n;i++) {
+        if (arr[i] == 0) {
+            j=i;
+            break;
         }
     }
-    while(cnt>0){ 
-        v.push_back(0);
-        cnt--;
+    for(int i=j+1;i<n;i++){
+        if(arr[i]!=0){
+        swap(arr[i],arr[j]);
+        j++;
+        }
     }
-    for(auto it:v) cout<<it<<endl;
+    for(auto it:arr) cout<<it<<endl;
 }

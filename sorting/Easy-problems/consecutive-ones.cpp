648a6f3335bb8864bd8e3f1514dc05720f47a52c
eshaansingla <147623220+eshaansingla@users.noreply.github.com>
2024-08-21 17:19:52 +0530

    consecutive ones

--- sorting/Easy-problems/consecutive-ones.cpp
+++ sorting/Easy-problems/consecutive-ones.cpp
@@ -9,5 +9,16 @@ int main(){
         cout<<"Enter a number:";
         cin>>v[i];
     }
-    
+    int cnt=0;
+    int no=0;
+    for(int i=0;i<n;i++){
+        if(v[i]==1){
+            no++;
+            cnt=max(no,cnt);
+        }
+        else{
+            no=0;
+        }
+    }
+    cout<<"Max number of consecutive 1s are: "<<cnt<<endl;
 }

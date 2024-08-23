a45e14882a732a24f0e8a4fd5efd84083d5f81f8
eshaansingla <147623220+eshaansingla@users.noreply.github.com>
2024-08-23 16:07:07 +0530

    up

--- sorting/Easy-problems/sub-array.cpp
+++ sorting/Easy-problems/sub-array.cpp
@@ -13,19 +13,27 @@ int main(){
     cout<<"Enter sum:";
     cin>>sum;
     //brute force
-   /* int k;
+    /*int k;
     int cnt=0;
     int m=0;
+    int len=0;
+    int s;
     for(int j=0;j<n;j++){
         k=0;
     for(int i=j;i<n;i++){
             k+=arr[i];
             cnt++;
             if(k==sum){
+                s=i-j+1;
+                len=max(s,len);
                 m++;
+            }
+            else{
+                s=0;
             }
         }
 }
 cout<<"The maximum number of subarrays:"<<m<<endl;
-cout<<"Total subarrays:"<<cnt<<endl;*/
+cout<<"Total subarrays:"<<cnt<<endl;
+cout<<"Length of max array:"<<len<<endl;*/
 }
---
+++ sorting/Medium-problems/2-sum.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter number of elements:";
+    cin>>n;
+    int arr[n];
+    int sum;
+    for(int i=0;i<n;i++){
+        cout<<"Enter a number:";
+        cin>>arr[i];
+    }
+    cout<<"Enter sum:";
+    cin>>sum;
+    
+    }

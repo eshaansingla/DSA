8ec39785572f3a0673ed149d3d5285622ba0550b
eshaansingla <147623220+eshaansingla@users.noreply.github.com>
2024-08-26 19:44:25 +0530

    ROW PRINTING

--- sorting/Hard-problems/pascal.cpp
+++ sorting/Hard-problems/pascal.cpp
@@ -1,7 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 // the element = rows-1 C columns-1
-
+//brute
+/*
 int facto(int n){
     if(n==1||n==0) return 1;
     else return n*facto(n-1);
@@ -10,8 +11,6 @@ int facto(int n){
 int myf(int r,int c){
 return facto(r-1)/(facto(c-1)*facto(r-c));
 }
-//brute
-/*
 void rowprinter(){
     int row,column;
     cout<<"Enter row:";
@@ -19,13 +18,38 @@ void rowprinter(){
     column=row;
     for(int i=1;i<=column;i++) cout<<myf(row,i)<<" ";
 }
-*/
+
+vector<vector<int>> generate(int numRows) {
+        int col=1;
+        vector<int>v;
+        vector<vector<int>>ans;
+        for(int i=1;i<=numRows;i++){
+            for(int j=1;j<=col;j++){
+                v.push_back(myf(i,j));
+            }
+            col++;
+            ans.push_back(v);
+            v.clear();
+        }
+        return ans;
+    }
+    */
+   void rowoptimal(){
+    int row,column;
+    cout<<"Enter row:";
+    cin>>row;
+    column=row;
+}
+
 int main(){
     int row,column;
     cout<<"Enter number of rows:";
     cin>>row;
     cout<<"Enter number of columns:";
     cin>>column;
-    cout<<"THE ELEMENT IS: "<<myf(row,column)<<endl;
-    rowprinter();
+    //cout<<"THE ELEMENT IS: "<<myf(row,column)<<endl;
+    //rowprinter();
+    //vector<vector<int>>Arrayrows=generate(row);
+
+
 }

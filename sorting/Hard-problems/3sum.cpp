711f144ec4a90b033f7b99bd20632207040da9d8
eshaansingla <147623220+eshaansingla@users.noreply.github.com>
2024-08-27 14:32:28 +0530

    optimal 3sum FINALLLYYY

--- sorting/Hard-problems/3sum.cpp
+++ sorting/Hard-problems/3sum.cpp
@@ -28,4 +28,59 @@ int main(){
             v.push_back(triplet);
         }
     */
+
+   //better
+   /*vector<vector<int>>v;
+        set<vector<int>> uniqueTriplets;
+        vector<int> triplet;
+        int search;
+    for(int i=0;i<n;i++){
+        set<int>st;
+        for(int j=i+1;j<n;j++){
+            search=(nums[i]+nums[j])*-1;
+                if(nums[j]==search && i!=j){
+                    triplet = {nums[i], nums[j],search};
+                        sort(triplet.begin(), triplet.end());
+                        uniqueTriplets.insert(triplet);
+                }
+                st.insert(nums[j]);
+        }
+    }
+     for (auto triplet : uniqueTriplets) {
+            v.push_back(triplet);
+        }
+    */
+   //optimal
+        vector<vector<int>>ans;
+        sort(nums,nums+n);
+        int j,k,sum;
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            j=i+1;
+            k=n-1;
+            while(j<k){
+                sum=nums[i]+nums[j]+nums[k];
+                if(sum>0){
+                    k--;
+                }
+                else if(sum<0){
+                    j++;
+                }
+                else{
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;  
+                }
+            }
+        }
+        for(int i=0;i<ans.size();i++){
+            for(int p=0;p<3;p++){
+                cout<<ans[i][p]<<",";
+            }
+            cout<<endl;
+        }
+   
+
 }

29689921723ec182c52173d48cfc0033758ba8ff
eshaansingla <147623220+eshaansingla@users.noreply.github.com>
2024-08-27 18:28:15 +0530

    final foursum

--- sorting/Hard-problems/4sum.cpp
+++ sorting/Hard-problems/4sum.cpp
@@ -65,4 +65,35 @@ int main(){
             cout<<endl;
         }
         */
-}
+    //optimal
+       int target;
+       vector<vector<int>>ans;
+       sort(nums,nums+n);
+       for(int i=0;i<n;i++){
+        if(i>0 && nums[i]==nums[i-1]) continue;
+        for(int j=i+1;j<n;j++){
+             if(j>i+1 && nums[j]==nums[j-1]) continue;
+                int k=j+1;
+                int l=n-1;
+                while(l>k){
+                    long long sum=(long long)nums[i]+nums[j]+nums[k]+nums[l];
+                    if(sum>target) l--;
+                    else if(sum<target) k++;
+                    else{
+                        ans.push_back({nums[i],nums[j],nums[i],nums[i]});
+                        l--;
+                        k++;
+                        while(l>k && nums[l]==nums[l+1]) l--;
+                        while(l>k && nums[k]==nums[k-1]) k++;
+                    }
+                }
+               
+            }
+    }
+    for(int i=0;i<ans.size();i++){
+            for(int p=0;p<4;p++){
+                cout<<ans[i][p]<<",";
+            }
+            cout<<endl;
+        }
+    }

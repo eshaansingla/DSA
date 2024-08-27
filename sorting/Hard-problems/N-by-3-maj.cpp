0028d55ccd463bd6858c1de7232b16cb91db0a89
eshaansingla <147623220+eshaansingla@users.noreply.github.com>
2024-08-27 11:15:47 +0530

    optimal n/3 majority

--- sorting/Hard-problems/N-by-3-maj.cpp
+++ sorting/Hard-problems/N-by-3-maj.cpp
@@ -12,29 +12,29 @@ int main(){
     //brute
     /*
         int max=INT_MIN;
-        sort(nums.begin(),nums.end());
+        sort(arr.begin(),arr.end());
         int cnt1=0;
         int cnt2=0;
         int s;
         int m1,m2;
         for(int i=0;i<n;i++){
-                s=nums[i];
+                s=arr[i];
             for(int j=0;j<n;j++){
-                if(nums[j]==nums[i]) cnt1++;
+                if(arr[j]==arr[i]) cnt1++;
             }
             if(cnt1>n/3){ 
-                m1=nums[i];
+                m1=arr[i];
                 break;
             }
             cnt1=0;
         }
         for(int i=0;i<n;i++){
-                s=nums[i];
+                s=arr[i];
             for(int j=0;j<n;j++){
-                if(nums[j]==nums[i]) cnt2++;
+                if(arr[j]==arr[i]) cnt2++;
             }
             if(cnt2>n/3 && s!=m1){ 
-                m2=nums[i];
+                m2=arr[i];
                 break;
             }
             cnt2=0;
@@ -52,7 +52,7 @@ int main(){
         }
 
     set<int>at;
-        map<int,int>hash;
+    map<int,int>hash;
     for(int i=0;i<n;i++){
         hash[arr[i]]+=1;
         if(hash[arr[i]]>n/3){
@@ -63,8 +63,34 @@ int main(){
     for(auto it:maj) cout<<it<<" ";
     }
     */
-   
+
    // optimal
-
-
-}
+   int cnt1=0,cnt2=0;
+        int el1,el2;
+        for(int i=0;i<n;i++){
+            if(cnt1==0 && arr[i]!=el2){
+                cnt1=1;
+                el1=arr[i];
+            }
+            else if(cnt2==0 && arr[i]!=el1){
+                cnt2=1;
+                el2=arr[i];
+            }
+            else if(arr[i]==el1) cnt1++;
+            else if(arr[i]==el2) cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+        cnt1=0;
+        cnt2=0;
+        for(int i=0;i<n;i++){
+            if(el1==arr[i]) cnt1++;
+            if(el2==arr[i]) cnt2++;
+        }
+        if(cnt1>n/3 && cnt2>n/3) cout<<el1<<" "<<el2<<endl;
+        if(cnt1>n/3)cout<<el1<<endl;
+        if(cnt2>n/3)cout<<el2<<endl;
+        else cout<<"No element"<<endl;
+    }

1dc55d257d356f11f3e007e5b2e1552a57157a19
eshaansingla <147623220+eshaansingla@users.noreply.github.com>
2024-09-01 13:38:50 +0530

    BS rec

--- Binary Search/BS-on-1D.cpp
+++ Binary Search/BS-on-1D.cpp
@@ -1,5 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
+//recursion
+int func(vector<int>& nums, int target,int low,int high){
+        int mid=(low+high)/2;
+        if(low>high) return -1;
+        else if(target==nums[mid]) return mid;
+        else if(nums[mid]>target) return func(nums,target,low,mid-1);
+        return func(nums,target,mid+1,high);
+    }
 int main(){
     vector<int>nums(5);
     int target;
@@ -12,6 +20,8 @@ int main(){
      cin>>target;
         int low=0;
         int high=n-1;
+    cout<< func(nums,target,low,high);
+        /*
         while(low<=high){
             int mid=(low+high)/2;
             if(nums[mid]==target) {
@@ -25,5 +35,5 @@ int main(){
                 low=mid+1;
             }
         }
-        cout<< -1;
+        cout<< -1;*/
     }

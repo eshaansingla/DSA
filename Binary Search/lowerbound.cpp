80e57a43d332ee538e1b2f55d31e7bae1a4affa0
eshaansingla <147623220+eshaansingla@users.noreply.github.com>
2024-09-01 15:39:45 +0530

    Lower bound in sorted array

---
+++ Binary Search/lowerbound.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int fun(vector<int>&nums,int target,int low,int high){
+int ans=high+1;
+int mid;
+if(low>=high) return -1;
+while(low<=high){
+mid=low + (high-low)/2;
+if(nums[mid]<target){ 
+    low=mid+1;
+}
+else{
+    ans=mid;
+    high=mid-1;
+}
+}
+return mid;
+}
+int main(){
+    int n;
+    cout<<"Enter n:";
+    cin>>n;
+    vector<int>nums(n);
+    int target;
+    for(int i=0;i<n;i++){
+       cout<<"Enter a number:";
+       cin>>nums[i];
+    }
+    cout<<"Enter the target:";
+    cin>>target;
+    int sol=fun(nums,target,0,n-1);
+    cout<<sol<<endl;
+}

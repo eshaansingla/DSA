20977b8fa38469f2aa0d6f01fd0b78ea4cf0958b
eshaansingla <147623220+eshaansingla@users.noreply.github.com>
2024-09-01 17:05:50 +0530

    floor and ceil

---
+++ Binary Search/floor-and-ceil-ops.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+int flow(vector<int> &nums,int target,int low,int high){
+int mid;
+int ans=-1;
+while(low<=high){
+    mid=low + (high-low)/2;
+if(nums[mid]<=target){
+    ans=nums[mid];
+    low=mid+1;
+}
+else high=mid-1;
+}
+return ans;
+}
+int roof(vector<int> &nums,int target,int low,int high){
+int mid;
+int ans=-1;
+while(low<=high){
+    mid=low + (high-low)/2;
+if(nums[mid]<target){
+    low=mid+1;
+}
+else {
+    ans=nums[mid];
+    high=mid-1;
+}
+}
+return ans;
+}
+int main(){
+    int n;
+    cout<<"Enter n:";
+    cin>>n;
+    vector<int>nums={4,5,9,11,23,27,35,38,45,47,49};
+    int target=21;
+    for(int i=0;i<n;i++){
+       cout<<"Enter a number:";
+       cin>>nums[i];
+    }
+    cout<<"Enter the target:";
+    cin>>target;
+    cout<<"Closest lower or equal value: "<<flow(nums,target,0,n-1)<<endl;
+    cout<<"Closest higher or equal value: "<<roof(nums,target,0,n-1)<<endl;
+}

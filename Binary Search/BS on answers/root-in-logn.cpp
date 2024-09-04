35084f4479023f70a1e9e6f9a4a80112867c650b
eshaansingla <147623220+eshaansingla@users.noreply.github.com>
2024-09-04 16:58:39 +0530

    Root optimal

--- Binary Search/BS on answers/root-in-logn.cpp
+++ Binary Search/BS on answers/root-in-logn.cpp
@@ -4,6 +4,8 @@ int main(){
     int x;
     cout<<"Enter a number: ";
     cin>>x;
+    //brute
+    /*
     long long product;
        if(x==1) return 1;
        for(long long j=0;j<=x/2;j++){
@@ -13,4 +15,21 @@ int main(){
         else break;
        }
        cout<<product<<endl;
-    }
+    */
+   //optimal
+
+       int low=1;
+       int high=x;
+       long long mid;
+       int ans;
+       if(x==0) return 0;
+       while(low<=high){
+        mid= low + (high-low)/2;
+        if(mid*mid>x) high=mid-1;
+        else{
+            ans=mid;
+            low=mid+1;
+        }
+       }
+       return ans;
+   }

f744d6c742d3c0ec30ca9ba248d5c2f971e45b43
eshaansingla <147623220+eshaansingla@users.noreply.github.com>
2024-09-05 15:08:16 +0530

    nth root optimal

--- Binary Search/BS on answers/nth-root.cpp
+++ Binary Search/BS on answers/nth-root.cpp
@@ -1,4 +1,13 @@
 #include<bits/stdc++.h>
+//optimal
+int power(int a,int p){
+    int ans=1;
+while(p>0){ 
+    ans*=a;
+    p--;
+}
+return ans;
+}
 using namespace std;
 int main(){
     int n;
@@ -8,10 +17,35 @@ int main(){
     cout<<"Enter x:";
     cin>>x;
     int ans=1;
+    //brute 
+    /*
     if(x==1) cout<<1;
     for(int i=2;i<x/2;i++){
         if(pow(i,n)<=x) ans=i;
         else break;
     }
+    cout<<ans<<endl;
+    */
+
+   //optimal
+   int low=1;
+   int high=x/2;
+   int mid;
+   int pw;
+   if(x==1 || x==0) cout<<x<<endl;
+   while(low<=high){
+    mid=low + (high-low)/2;
+    pw=power(mid,n);
+    if(pw==x){
+        ans=mid;
+        cout<<ans<<endl;
+        break;
+    }
+    else if(pw>x) high=mid-1;
+    else {
+        low=mid+1;
+        ans=mid;
+        }
+   }
     cout<<ans<<endl;
 }

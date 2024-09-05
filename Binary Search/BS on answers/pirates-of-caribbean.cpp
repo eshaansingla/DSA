6a35147e6433fab42f538c632163cba579baafc5
eshaansingla <147623220+eshaansingla@users.noreply.github.com>
2024-09-05 18:05:02 +0530

    pirates 💀

---
+++ Binary Search/BS on answers/pirates-of-caribbean.cpp
@@ -0,0 +1,52 @@
+/*
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠀⠤⠴⠶⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣶⣾⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠂⠉⡇⠀⠀⠀⢰⣿⣿⣿⣿⣧⠀⠀⢀⣄⣀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢠⣶⣶⣷⠀⠀⠀⠸⠟⠁⠀⡇⠀⠀⠀⠀⠀⢹⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠘⠟⢹⣋⣀⡀⢀⣤⣶⣿⣿⣿⣿⣿⡿⠛⣠⣼⣿⡟⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣴⣾⣿⣿⣿⣿⢁⣾⣿⣿⣿⣿⣿⣿⡿⢁⣾⣿⣿⣿⠁⠀⠀⠀⠀
+⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⢸⣿⣿⣿⣿⣿⣿⣿⡇⢸⣿⣿⣿⠿⠇⠀⠀⠀⠀
+⠀⠀⠀⠳⣤⣙⠟⠛⢻⠿⣿⠸⣿⣿⣿⣿⣿⣿⣿⣇⠘⠉⠀⢸⠀⢀⣠⠀⠀⠀
+⠀⠀⠀⠀⠈⠻⣷⣦⣼⠀⠀⠀⢻⣿⣿⠿⢿⡿⠿⣿⡄⠀⠀⣼⣷⣿⣿⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣶⣄⡈⠉⠀⠀⢸⡇⠀⠀⠉⠂⠀⣿⣿⣿⣧⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣷⣤⣀⣸⣧⣠⣤⣴⣶⣾⣿⣿⣿⡿⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠟⠛⠉⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+*/
+#include<bits/stdc++.h>
+using namespace std;
+long long sumation(vector<int>& weights, int cap){
+   int days = 1; 
+    int load = 0;
+    int n = weights.size(); 
+    for (int i = 0; i < n; i++) {
+        if (load + weights[i] > cap) {
+            days += 1; 
+            load = weights[i]; 
+        }
+        else {
+           
+            load += weights[i];
+        }
+    }
+    return days;
+}
+    int shipWithinDays(vector<int>& weights, int days) {
+        int n=weights.size();
+        int low=*max_element(weights.begin(),weights.end());
+        int high=0;
+        for(int i=0;i<n;i++) high+=weights[i];
+        while(low<=high){
+            int mid=low + (high-low)/2;
+            int tdays=sumation(weights,mid);
+            if(tdays<=days) high=mid-1;
+            else low=mid+1;
+        }
+        return low;
+    }
+int main(){
+vector<int>weights={1,2,3,5,4,6,5,7,9,8,8,2,10};
+int days=10;
+cout<<shipWithinDays(weights,days)<<endl;
+}

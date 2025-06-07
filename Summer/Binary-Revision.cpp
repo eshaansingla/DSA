33ba886fd5a0bc6edd054adeb5383983982f7310
Eshaan Singla <eshaansingla2807@gmail.com>
2025-06-07 15:28:55 +0530

    🙌

--- Summer/Binary-Revision.cpp
+++ Summer/Binary-Revision.cpp
@@ -70,6 +70,109 @@ int peak(vector<int> &nums)
     }
     return -1;
 }
+void reversed(vector<int> &arr, int m)
+{
+    int i = m + 1;
+    int j = arr.size() - 1;
+    while (i <= j)
+    {
+        swap(arr[i++], arr[j--]);
+    }
+}
+vector<int> merge2sorted(vector<int> &arr1, vector<int> &arr2, int n, int m)
+{
+    vector<int> ans;
+    int i = 0, j = 0;
+    while (i < n && j < m)
+    {
+        if (arr1[i] < arr2[j])
+        {
+            ans.push_back(arr1[i++]);
+        }
+        else
+            ans.push_back(arr2[j++]);
+    }
+    while (i < n)
+        ans.push_back(arr1[i++]);
+    while (j < m)
+        ans.push_back(arr2[j++]);
+    return ans;
+}
+void remove0s(vector<int> &nums)
+{
+    int nonzeros = 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] != 0)
+        {
+            swap(nums[nonzeros++], nums[i]);
+        }
+    }
+}
+void rotateArray(vector<int> &vec, int k)
+{
+    // 1 2 3 7 9
+    // 7 9 1 2 3
+    // 3 2 1 7 9
+    // 3 2 1 9 7
+    // 7 9 1 2 3
+    reverse(vec.begin(), vec.begin() + k);
+    reverse(vec.begin() + k, vec.end());
+    reverse(vec.begin(), vec.end());
+}
+vector<int> addition(vector<int> &a, vector<int> &b)
+{
+    vector<int> ans(max(a.size(), b.size()) + 1, 0);
+    int i = a.size() - 1;
+    int j = b.size() - 1;
+    int k = ans.size() - 1;
+    int carry = 0;
+    while (i >= 0 && j >= 0)
+    {
+        int temp = a[i--] + b[j--] + carry;
+        if (temp >= 10)
+        {
+            carry = (temp - temp % 10) / 10;
+            temp = temp % 10;
+        }
+        else
+            carry = 0;
+        ans[k--] = (temp);
+    }
+    while (i >= 0)
+    {
+        int temp = a[i--] + carry;
+        if (temp >= 10)
+        {
+            carry = (temp - temp % 10) / 10;
+            temp = temp % 10;
+        }
+        else
+            carry = 0;
+        ans[k--] = (temp);
+    }
+    while (j >= 0)
+    {
+        int temp = b[j--] + carry;
+        if (temp >= 10)
+        {
+            carry = (temp - temp % 10) / 10;
+            temp = temp % 10;
+        }
+        else
+            carry = 0;
+        ans[k--] = (temp);
+    }
+    ans[k--] = carry;
+    carry = 0;
+    i = 0;
+    while (i < ans.size() && ans[0] == 0)
+    {
+        ans.erase(ans.begin());
+        i++;
+    }
+    return ans;
+}
 int main()
 {
     vector<int> vec = {1, 2, 3, 3, 3, 3, 4, 5};
@@ -78,4 +181,35 @@ int main()
     cout << lastocc(vec, 3) << endl;
     cout << lastocc(vec, 3) - firstocc(vec, 3) + 1 << endl;
     cout << p[peak(p)] << endl;
+    reversed(vec, 4);
+    for (auto it : vec)
+        cout << it << " ";
+    cout << endl;
+    vector<int> v1 = {2, 3, 3, 4, 4, 4, 4, 5, 6, 6, 6, 6, 77};
+    vector<int> v2 = {1, 2, 2, 2, 2, 5, 6, 6, 6, 7, 8, 9, 9, 9};
+    vector<int> ans = merge2sorted(v1, v2, v1.size(), v2.size());
+    for (auto it : ans)
+        cout << it << " ";
+    cout << endl;
+    vector<int> arr = {0, 1, 0, 0, 0, 3, 4, 0, 0, 2, 3, 4, 0, 0, 0, 1, 0, 0};
+    remove0s(arr);
+    for (auto it : arr)
+    {
+        cout << it << " ";
+    }
+    cout << endl;
+    vector<int> a = {1, 2, 3, 7, 9};
+    rotateArray(a, 3);
+    for (auto it : a)
+        cout
+            << it << " ";
+    cout << endl;
+    vector<int> add1 = {9, 9, 9};
+    vector<int> add2 = {9, 9, 9};
+    vector<int> add = addition(add2, add1);
+    for (auto it : add)
+    {
+        cout << it << " ";
+    }
+    cout << endl;
 }
